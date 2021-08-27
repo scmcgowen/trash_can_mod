ad21b1aa88e8f9a0929ae38502940f971063c057
BuildTools <unconfigured@null.spigotmc.org>
2021-08-26 19:28:17 -0700

    message

---
+++ src/main/java/com/herrkatze/trashcan/blocks/trashcanblock.java
@@ -0,0 +1,9 @@
+package com.herrkatze.trashcan.blocks;
+
+import net.minecraft.block.Block;
+
+public class trashcanblock extends Block {
+    public trashcanblock(Properties properties) {
+        super(properties);
+    }
+}
